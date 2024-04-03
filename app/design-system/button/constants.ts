dcb775dff665d1e348a8f6e3abeafc9eedf4e567
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-04-03 14:46:23 +0530

    Update constants.ts

--- app/design-system/button/constants.ts
+++ app/design-system/button/constants.ts
@@ -1,1 +1,1 @@
-export const BUTTON_COMPONENT_GITHUB_URL = `https://github.com/JAYKALIA007/galleria`;
+export const BUTTON_COMPONENT_GITHUB_URL = `https://github.com/JAYKALIA007/galleria/tree/master/app/design-system/button`;
