5f9e7394cb01447115457f68c7b4eb25b578b345
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-04-04 18:25:14 +0530

    add meta data for the design system pages

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+  title: "Galleria | Design System",
+  description:
+    "A collection of reusable coded components, which can be used in your projects.",
+  keywords: ["frontend", "components", "design-system", "button"],
+};
+
 const DesignSystem: React.FC = () => (
   <div className="italic underline flex flex-col justify-start m-10 gap-20 w-fit p-4">
     <Link href={"/design-system/button"}>Button component</Link>
