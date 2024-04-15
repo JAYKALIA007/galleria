01d37dc67935012be5c8c78e059cffd59f731bfc
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-04-16 00:21:02 +0530

    make content take the full height of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark:bg-gray-950`}>
+      <body className={`${inter.className} dark:bg-gray-950 min-h-screen`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
