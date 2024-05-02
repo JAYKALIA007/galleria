34252ce2cf1bb422e5870af4ef895a9016aec64a
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-05-03 00:37:33 +0530

    add some style to the about page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,11 @@
 import Link from "next/link";
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
+import { usePathname } from "next/navigation";
+import { ABOUT_PAGE } from "../constants";
 
 export const Header: React.FC = () => {
+  const pathname = usePathname();
   return (
     <div className="m-2 p-2 relative flex justify-between items-center">
       <div className="w-full gap-28 sm:gap-20 md:gap-96 lg:gap-40 xl:gap-96 flex items-baseline mx-2 md:mx-10">
@@ -11,8 +14,11 @@ export const Header: React.FC = () => {
           Home
         </Link>
         <Link
-          href="/about"
-          className="hover:text-blue-500 hover:underline underline-offset-4 "
+          href={ABOUT_PAGE}
+          className={`hover:text-blue-700 hover:dark:text-blue-500 hover:underline underline-offset-4 ${
+            pathname === ABOUT_PAGE &&
+            "text-blue-700 dark:text-blue-500 underline"
+          }`}
         >
           @jay_prakash07
         </Link>
--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,2 @@
 export const SHADCN_UI_URL = "https://ui.shadcn.com/";
+export const ABOUT_PAGE = "/about";
