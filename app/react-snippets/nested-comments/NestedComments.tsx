a3bdb33ada6b440faeb320a3e7d8ae01a1515c6f
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-06-07 03:26:37 +0530

    memoize

--- app/react-snippets/nested-comments/NestedComments.tsx
+++ app/react-snippets/nested-comments/NestedComments.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { ButtonComponent } from "@/app/design-system/button/ButtonComponent";
 import {
   ButtonSizeVariantType,
@@ -6,7 +7,7 @@ import {
 } from "@/app/design-system/button/types";
 import { Input } from "@/components/ui/input";
 import { uuidv4 } from "@/lib/utils";
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import {
   CommentsType,
   MOCK_COMMENTS_DATA,
@@ -16,7 +17,6 @@ import {
 import { SlideInAnimationContainer } from "@/app/components/SlideInAnimationContainer";
 import Link from "next/link";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import { ExternalLinkIcon } from "lucide-react";
 
 export const NestedComments = () => {
   const [comments, setComments] = useState<CommentsType>(MOCK_COMMENTS_DATA);
@@ -73,14 +73,13 @@ export const NestedComments = () => {
           </div>
           <div>
             {comments.map((comment) => (
-              <div key={comment.id}>
-                <Comment
-                  comment={comment}
-                  comments={comments}
-                  onChange={setComments}
-                  isParent
-                />
-              </div>
+              <Comment
+                key={comment.id}
+                comment={comment}
+                comments={comments}
+                onChange={setComments}
+                isParent
+              />
             ))}
           </div>
         </div>
@@ -89,96 +88,107 @@ export const NestedComments = () => {
   );
 };
 
-const Comment = ({
-  comment,
-  comments,
-  onChange,
-  isParent,
-}: {
-  comment: CommentsType[0];
-  comments: CommentsType;
-  onChange: (comments: CommentsType) => void;
-  isParent: boolean;
-}) => {
-  const [reply, setReply] = useState("");
-  const [showReplyTextBox, setShowReplyTextBox] = useState(false);
+const Comment = React.memo(
+  ({
+    comment,
+    comments,
+    onChange,
+    isParent,
+  }: {
+    comment: CommentsType[0];
+    comments: CommentsType;
+    onChange: (comments: CommentsType) => void;
+    isParent: boolean;
+  }) => {
+    const [reply, setReply] = useState("");
+    const [showReplyTextBox, setShowReplyTextBox] = useState(false);
 
-  const hasReplies = useMemo(
-    () => (comment.replies && comment.replies.length > 0) || false,
-    [comment.replies]
-  );
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleChangeReply = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setReply(e.target.value);
-  };
+    const hasReplies = useMemo(
+      () => (comment.replies && comment.replies.length > 0) || false,
+      [comment.replies]
+    );
 
-  const handleAddReply = useCallback(() => {
-    const addedReply: CommentsType[0] = {
-      id: uuidv4(),
-      comment: reply,
-      replies: [],
+    const handleChangeReply = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setReply(e.target.value);
     };
 
-    const updatedComments = updateComments(comments, comment.id, addedReply);
-    onChange(updatedComments);
-    setShowReplyTextBox(false);
-    setReply("");
-  }, [comment.id, comments, onChange, reply]);
+    const handleAddReply = useCallback(() => {
+      const addedReply: CommentsType[0] = {
+        id: uuidv4(),
+        comment: reply,
+        replies: [],
+      };
 
-  return (
-    <div
-      className={`ml-1 p-4 ${
-        !isParent && "border-l border-gray-200 dark:border-gray-800"
-      }`}
-    >
-      <div className="flex gap-2 items-center">
-        <div>{comment.comment}</div>
-        {!showReplyTextBox && (
-          <button
-            onClick={() => setShowReplyTextBox(true)}
-            className="text-[10px] hover:underline underline-offset-4 text-blue-500"
-          >
-            Reply
-          </button>
-        )}
-      </div>
-      {showReplyTextBox && (
-        <SlideInAnimationContainer>
-          <div className="flex gap-4 items-center p-4">
-            <Input
-              value={reply}
-              onChange={handleChangeReply}
-              placeholder="Add a reply..."
-            />
-            <ButtonComponent
-              onClick={handleAddReply}
-              theme={ButtonThemeVariantType.OUTLINE}
-              size={ButtonSizeVariantType.XS}
-              disabled={!reply}
+      const updatedComments = updateComments(comments, comment.id, addedReply);
+      onChange(updatedComments);
+      setShowReplyTextBox(false);
+      setReply("");
+    }, [comment.id, comments, onChange, reply]);
+
+    return (
+      <div
+        className={`ml-1 p-4 ${
+          !isParent && "border-l border-gray-200 dark:border-gray-800"
+        }`}
+      >
+        <div className="flex gap-2 items-center">
+          <div>{comment.comment}</div>
+          {!showReplyTextBox && (
+            <button
+              onClick={() => setShowReplyTextBox(true)}
+              className="text-[10px] hover:underline underline-offset-4 text-blue-500"
             >
               Reply
-            </ButtonComponent>
-          </div>
-        </SlideInAnimationContainer>
-      )}
-      {hasReplies &&
-        comment.replies.map((reply) => (
-          <Comment
-            key={reply.id}
-            comment={reply}
-            comments={comment.replies}
-            onChange={(updatedReplies) => {
-              const updatedComments = comments.map((currComment) =>
-                currComment.id === comment.id
-                  ? { ...comment, replies: updatedReplies }
-                  : currComment
-              );
+            </button>
+          )}
+        </div>
+        {showReplyTextBox && (
+          <SlideInAnimationContainer>
+            <div className="flex gap-4 items-center p-4">
+              <Input
+                value={reply}
+                onChange={handleChangeReply}
+                placeholder="Add a reply..."
+                ref={inputRef}
+              />
+              <ButtonComponent
+                onClick={handleAddReply}
+                theme={ButtonThemeVariantType.OUTLINE}
+                size={ButtonSizeVariantType.XS}
+                disabled={!reply}
+              >
+                Reply
+              </ButtonComponent>
+            </div>
+          </SlideInAnimationContainer>
+        )}
+        {hasReplies &&
+          comment.replies.map((reply) => (
+            <MemoizedComment
+              key={reply.id}
+              comment={reply}
+              comments={comment.replies}
+              onChange={(updatedReplies) => {
+                const updatedComments = comments.map((currComment) =>
+                  currComment.id === comment.id
+                    ? { ...comment, replies: updatedReplies }
+                    : currComment
+                );
 
-              onChange(updatedComments);
-            }}
-            isParent={false}
-          />
-        ))}
-    </div>
-  );
-};
+                onChange(updatedComments);
+              }}
+              isParent={false}
+            />
+          ))}
+      </div>
+    );
+  }
+);
+
+Comment.displayName = "Comment";
+
+const MemoizedComment = React.memo(Comment);
+
+export default NestedComments;
