68929f2d3cb8f26f0e6e08ad13119a5c7dab9edf
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-06-27 00:26:32 +0530

    added meta data

--- app/react-snippets/add-tags/page.tsx
+++ app/react-snippets/add-tags/page.tsx
@@ -1,7 +1,23 @@
 import { AddTagsContainer } from "./AddTagsContainer";
 
-const AddTagsWrapper = () => {
+export const metadata = {
+  title: "Add Tags | React Snippets",
+  description: `A chip component which shows suggestions as you type and where you can
+          add & remove tags. Also supports backspace keydown event to remove a
+          tag from the selected tags.`,
+  keywords: [
+    "add-tags",
+    "chip-component",
+    "react-snippets",
+    "web development",
+    "frontend",
+    "snippets",
+    "react.js",
+  ],
+};
+
+const AddTagsPage = () => {
   return <AddTagsContainer />;
 };
 
-export default AddTagsWrapper;
+export default AddTagsPage;
