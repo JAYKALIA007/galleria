5998f040b5de39186dce73faaf3d1bc8b1a593a8
jaykalia18 <jay.k@thesilverlabs.com>
2024-12-02 01:38:48 +0530

    added analytics

--- app/constants.ts
+++ app/constants.ts
@@ -1,6 +1,6 @@
 export const SHADCN_UI_URL = "https://ui.shadcn.com/";
 export const ABOUT_PAGE = "/about";
-
+export const GOOGLE_TRACKING_ID = `G-V1B2CHF6EP`;
 export const ABOUT_CONTENT = `You can find me inspecting elements on random websites, crafting
 intuitive seamless interfaces, and preaching the good word of design
 systems. My focus lies in user flow, interaction, and experience. I
