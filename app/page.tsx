9e85f6812bc3f358f4c4d82a80a1675776fe9f26
JAYKALIA007 <jaykalia047@gmail.com>
2025-03-18 21:20:40 +0530

    copy changes

--- app/page.tsx
+++ app/page.tsx
@@ -48,8 +48,11 @@ export default function Home() {
 
           <div className="flex flex-col gap-2">
             <div className="text-base md:text-lg font-semibold">My work</div>
-            <Link href="/blogs" className="hover:underline underline-offset-4">
-              Blogs
+            <Link
+              href="/writing"
+              className="hover:underline underline-offset-4"
+            >
+              Writing
             </Link>
             <Link
               href="/design-system"
